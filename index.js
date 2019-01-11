6714587b41d7d3a428c42d578dedb4db029bce75
Tammy Metz <tammy@tammymetz.com>
2019-01-11 00:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ You should NOT try to redefine any of the above variables.
 var ageIsValid = Number.isInteger(currentAge);
 var randomNumber = Math.random() * 20;
 var randomInteger = Math.floor(randomNumber);
-var randomUserID = Math.floor(Math.random() * 20) + 1000000000
+var randomUserID = Math.floor(Math.random() * 20) + 1000000001
